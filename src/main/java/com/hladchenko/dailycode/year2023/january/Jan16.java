97cb50110ad4bf34ab7a397da66520ba90b2c078
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-01-16 22:22:22 +0200

    init + jan16

---
+++ src/main/java/com/hladchenko/dailycode/year2023/january/Jan16.java
@@ -0,0 +1,8 @@
+package com.hladchenko.dailycode.year2023.january;
+
+public class Jan16 {
+
+  public static void main(String[] args) {
+    System.out.println("Якщо ти не пишеш код кожен день, а ти програміст - ти довбойоб, а не програміст.");
+  }
+}
